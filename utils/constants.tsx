589be5770e6c7d053160c353ff447a9bfe491bf1
rominavc99 <animor_0912@hotmail.com>
2022-11-17 05:54:48 -0700

    primer commit

---
+++ utils/constants.tsx
@@ -0,0 +1,42 @@
+import { BsCode, BsEmojiSunglasses } from 'react-icons/bs';
+import { GiCakeSlice, GiGalaxy, GiLipstick } from 'react-icons/gi';
+import { FaPaw, FaMedal, FaGamepad } from 'react-icons/fa';
+
+export const topics = [
+  {
+    name: 'Dibujo',
+    icon: <BsCode />,
+  },
+  {
+    name: 'Acuarela',
+    icon: <BsEmojiSunglasses />,
+  },
+  {
+    name: 'Oleo',
+    icon: <FaGamepad />,
+  },
+  {
+    name: 'Gis',
+    icon: <GiCakeSlice />,
+  },
+  {
+    name: 'Gis Pastel',
+    icon: <GiGalaxy />,
+  },
+  {
+    name: 'Collage',
+    icon: <GiLipstick />,
+  },
+  {
+    name: 'Dibujo digital',
+    icon: <FaPaw />,
+  },
+  {
+    name: 'Foto',
+    icon: <FaMedal />,
+  },
+];
+
+export const footerList1 = ['About', 'Newsroom', 'Store', 'Contact', 'Carrers', 'ByteDance', 'Creator Directory']
+export const footerList2 = [ 'TikTik for Good','Advertise','Developers','Transparency','TikTik Rewards' ]
+export const footerList3 = [ 'Help', 'Safety', 'Terms', 'Privacy', 'Creator Portal', 'Community Guidelines' ]
